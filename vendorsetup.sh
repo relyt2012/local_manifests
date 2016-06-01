f5eb23eb24e233badaa9098b59cc2ae218dbd000
FaArIsH <faarishhassan@gmail.com>
2016-06-01 21:47:27 +0500

    Add files via upload

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+for combo in $(curl -s  https://raw.githubusercontent.com/AICP/vendor_jenkins/mm6.0/aicp-build-targets | sed -e 's/#.*$//' | grep mm6.0 | awk {'print $2'})
+do
+    add_lunch_combo $combo
+done
+
+add_lunch_combo aicp_lt02ltespr-userdebug

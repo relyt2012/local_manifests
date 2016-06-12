c914d2924d1f22b7cdec3eb85c00e300c725f6ec
FaArIsH <faarishhassan@gmail.com>
2016-06-12 15:20:50 +0500

    Update lt02ltespr.mk

--- lt02ltespr.mk
+++ lt02ltespr.mk
@@ -1,8 +1,8 @@
 # Inherit common product files.
-$(call inherit-product, vendor/aicp/configs/common.mk)
+$(call inherit-product, vendor/aicp/configs/common_tablet.mk)
 
 # Inherit telephony stuff
-$(call inherit-product, vendor/aicp/configs/telephony.mk)
+$(call inherit-product, vendor/aicp/configs/common_tablet.mk)
 
 # Inherit AOSP device configuration for lt02ltespr.
 $(call inherit-product, device/samsung/lt02ltespr/full_lt02ltespr.mk)
